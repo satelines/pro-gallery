425a3062499d9e1c337f2b2284a4d6e4a9dd740f
ritabazak <rita.bazak@gmail.com>
2020-11-25 11:54:46 +0200

    precommit check

--- packages/layouts/src/logic/create-layout.js
+++ packages/layouts/src/logic/create-layout.js
@@ -1,5 +1,5 @@
 import Layouter from './layouter';
 
 export default function createLayout(...args) {
-   return new Layouter(...args).createLayout();
+    return new Layouter(...args).createLayout();
 }
